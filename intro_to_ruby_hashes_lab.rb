de178b78ab1b9eaedb3f272dd4024e71990ee35c
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-02 00:11:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,16 +9,16 @@ end
 
 def monopoly
 	monopoly = { 
-	  :railroads => {
-	    :pieces => 4
-	  }
+	  :railroads => {}
 	}
 
 end
 
 def monopoly_with_second_tier
 	monopoly = { 
-	  :railroads => {}
+	  :railroads => {
+	    :pieces => 4
+	  }
 	}
 
 end
